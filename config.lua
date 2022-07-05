3b75adf72b6fdf50f9d03454ed354d3c3c49dd72
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2022-07-05 18:45:54 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,15 @@
+Config = {}
 
+Config.MarkerType = 1
+Config.MarkerColor = {255, 255, 255}
+Config.MarkerSize = {2.0, 2.0, 1.0} 
+
+Config.Position = {
+    Initial = {
+        vector3(x, y, z)	
+    }, 
+    
+    Teleported = {
+        vector3(x, y, z)	
+    } 
+} 
