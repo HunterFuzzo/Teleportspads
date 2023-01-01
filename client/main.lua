ad884baeca39eb4a3d42ac1895c30f4120ac5573
Azuka <100121407+HunterFuzzo@users.noreply.github.com>
2023-01-01 17:05:02 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -9,42 +9,60 @@ RegisterCommand("coords", function(source, args, rawcommand)
 end)
 
 Citizen.CreateThread(function()
+    local isInTeleportZone = false
+
     while true do
-        for k, initalPos in pairs(Config.Position.Initial) do
-            for k, teleportedPos in pairs(Config.Position.Teleported) do
-                local playerPed = GetPlayerPed(PlayerPedId())
-                local playerPos = GetEntityCoords(playerPed)
-                local getDistance1 = GetDistanceBetweenCoords(playerPos, initialPos, true)
-                local getDistance2 = GetDistanceBetweenCoords(playerPos, teleportedPos, true)
-                local interval = 1
+        local playerPed = GetPlayerPed(PlayerPedId())
+        local playerPos = GetEntityCoords(playerPed)
+        local interval = 1
 
-                if getDistance1 < 20 then
-                    interval = 200
-                else
-                    interval = 1
-                    DrawMarker(Config.MarkerType, initalPos, 0.0, 0.0, 0.0, 180.0, 0.0, 0.0, Config.MarkerSize, Config.MarkerColor, 100, false, true, 2, false, false, false, false)
-                    if getDistance1 < 1 then
-                        ESX.ShowHelpNotification("Press ~INPUT_CONTEXT~ to teleport")
-                        if IsControlJustPressed(0, 51) then
-                            SetEntityCoords(playerPed, teleportedPos, true, false, false, false)
-                        end
-                    end
-                end 
-
-                if getDistance2 < 20 then
-                    interval = 200
-                else
-                    interval = 1
-                    DrawMarker(Config.MarkerType, teleportedPos, 0.0, 0.0, 0.0, 180.0, 0.0, 0.0, Config.MarkerSize, Config.MarkerColor, 100, false, true, 2, false, false, false, false)
-                    if getDistance2 < 1 then
-                        ESX.ShowHelpNotification("Press ~INPUT_CONTEXT~to teleport") 
-                        if IsControlJustPressed(0, 51) then
-                            SetEntityCoords(playerPed, initialPos, true, false, false, false)
-                        end
-                    end
+        for k, initialPos in pairs(Config.Position.Initial) do
+            local getDistance = GetDistanceBetweenCoords(playerPos, initialPos, true)
+            if getDistance < 20 then
+                interval = 200
+                if getDistance < 1 then
+                    isInTeleportZone = true
                 end
             end
         end
+
+        for k, teleportedPos in pairs(Config.Position.Teleported) do
+            local getDistance = GetDistanceBetweenCoords(playerPos, teleportedPos, true)
+            if getDistance < 20 then
+                interval = 200
+                if getDistance < 1 then
+                    isInTeleportZone = true
+                end
+            end
+        end
+
+        if isInTeleportZone then
+            ESX.ShowHelpNotification("Press ~INPUT_CONTEXT~ to teleport") 
+            if IsControlJustPressed(0, 51) then
+                isInTeleportZone = false
+                for k, initialPos in pairs(Config.Position.Initial) do
+                    local getDistance = GetDistanceBetweenCoords(playerPos, initialPos, true)
+                    if getDistance < 1 then
+                        SetEntityCoords(playerPed, Config.Position.Teleported[k], true, false, false, false)
+                    end
+                end
+
+                for k, teleportedPos in pairs(Config.Position.Teleported) do
+                    local getDistance = GetDistanceBetweenCoords(playerPos, teleportedPos, true)
+                    if getDistance < 1 then
+                        SetEntityCoords(playerPed, Config.Position.Initial[k], true, false, false, false)
+                    end
+                end
+            end
+        else
+            for k, initialPos in pairs(Config.Position.Initial) do
+                DrawMarker(Config.MarkerType, initialPos, 0.0, 0.0, 0.0, 180.0, 0.0, 0.0, Config.MarkerSize, Config.MarkerColor, 100, false, true, 2, false, false, false, false)
+            end
+            for k, teleportedPos in pairs(Config.Position.Teleported) do
+                DrawMarker(Config.MarkerType, teleportedPos, 0.0, 0.0, 0.0, 180.0, 0.0, 0.0, Config.MarkerSize, Config.MarkerColor, 100, false, true, 2, false, false, false, false)
+            end
+        end
+
         Citizen.Wait(interval)
     end
 end)
